17f5e45a9686b148ac6b028e3ca383c1e94bfc52
CuongNM <cuong.nm172989@sis.hust.edu.vn>
2022-02-26 12:15:39 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StatisticComponent } from './statistic/statistic.component';
+import { TableCustomerComponent } from './statistic/table-customer/table-customer.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StatisticComponent,
+    TableCustomerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
